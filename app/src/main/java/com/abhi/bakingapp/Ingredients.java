46740e6c05a07bd95309bf1b102b41e01776d738
AbhilashReddy17 <abhilashreddy.sr@gmail.com>
2018-10-19 00:57:00 -0500

    added the ingredients

---
+++ app/src/main/java/com/abhi/bakingapp/Ingredients.java
@@ -0,0 +1,34 @@
+package com.abhi.bakingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import adapters.IngredientAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import models.SingletonClass;
+
+import static com.abhi.bakingapp.MainActivity.RECIPE_CLICKED;
+
+public class Ingredients extends AppCompatActivity {
+
+    @BindView(R.id.ingredient_recyclerview_id)
+    RecyclerView recyclerView;
+
+    IngredientAdapter  ingredientAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingredients);
+        ButterKnife.bind(this);
+
+        int recipePosition = getIntent().getIntExtra(RECIPE_CLICKED,0);
+        LinearLayoutManager manager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(manager);
+        ingredientAdapter = new IngredientAdapter(this, SingletonClass.getsInstance().getRecipes().get(recipePosition).getIngredients());
+        recyclerView.setAdapter(ingredientAdapter);
+
+    }
+}
